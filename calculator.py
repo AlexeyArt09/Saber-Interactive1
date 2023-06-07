5182af27cecff7831fb5c0ae9153f7eb8ee849f2
AlexeyArt09 <133880740+AlexeyArt09@users.noreply.github.com>
2023-06-07 11:48:47 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+def calculate(expression):
+    try:
+        result = eval(expression)  
+        return result
+    except Exception as e:
+        return "Ошибка: " + str(e)
+expression = input("Введите математическое выражение: ")
+result = calculate(expression)
+print("Результат:", result)
